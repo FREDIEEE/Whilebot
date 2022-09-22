d52219ac272bbc58aead8259a4a422b0b7055dd0
Alfredo Alvarado <fredieee21@gmail.com>
2022-09-21 21:07:56 -0500

    ForeverAPI

---
+++ ForeverAPI.py
@@ -0,0 +1,22 @@
+from time import sleep, time, localtime,strftime
+import threading
+import logging
+
+global USERS
+logging.basicConfig(level=logging.INFO, format='%(levelname)s-%(message)s')
+
+def hilo_time():
+    while True:
+        TimeNow = strftime("%H:%M:%S", localtime())
+        logging.info(f"ALF_API: Corriendo servidor x tiempo {TimeNow}")
+        sleep(30) #Cada 5 min
+
+
+if __name__ == "__main__":
+
+    logging.info("ALF_API: Iniciando servicio infinito")
+    hilo = threading.Thread(name="hilo_webhook", target=hilo_time)
+    hilo.start()
+    while True:
+        sleep(1)
+        TimeNow = strftime("%H:%M:%S", localtime())
---
+++ settings.py
@@ -0,0 +1,11 @@
+import os
+#from . import secret
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DRBUG = False
+
+ALLOWED_HOST = ["*"]
+
+#INSTALLED APPS = []
+
