bd7bc3ee5b350794388a7d857433f199c8c2dfde
Alfredo Alvarado <fredieee21@gmail.com>
2022-09-21 22:05:30 -0500

    ForeverAPI

--- ForeverAPI.py
+++ ForeverAPI.py
@@ -1,11 +1,23 @@
 from time import sleep, time, localtime,strftime
+from flask import Flask, request, jsonify
+from waitress import serve
 import threading
 import logging
 import os
 import sys
 
-global USERS
 logging.basicConfig(level=logging.INFO, format='%(levelname)s-%(message)s')
+
+web_server = Flask(__name__)
+
+@web_server.route("/download", methods=["GET"])
+def testret():
+    return jsonify({"response": "HELLO WORLD "})
+
+def hilo_server():
+    while True:
+        serve(web_server, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+        sleep(5)
 
 def hilo_time():
     while True:
@@ -19,6 +31,8 @@ if __name__ == "__main__":
     print("ALF_API: Iniciando servicio infinito")
     hilo = threading.Thread(name="hilo_webhook", target=hilo_time)
     hilo.start()
+    hilo2 = threading.Thread(name="hilo_webhook", target=hilo_server)
+    hilo2.start()
     while True:
         sleep(1)
         TimeNow = strftime("%H:%M:%S", localtime())
